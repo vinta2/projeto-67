1249989ee80caba27a7ee6654cb205582451e51c
vinta2 <102837440+vinta2@users.noreply.github.com>
2023-08-06 17:10:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+// adicione SDK do Firebase
+var firebaseConfig = {
+    const firebaseConfig = {
+    apiKey: "AIzaSyC8NXHWEybmxxCuHy5890Kip4Z9orhKEIU",
+    authDomain: "aula-teste-b7e92.firebaseapp.com",
+    projectId: "aula-teste-b7e92",
+    storageBucket: "aula-teste-b7e92.appspot.com",
+    messagingSenderId: "691268158592",
+    appId: "1:691268158592:web:b9eb63e2f714c2417c5265"
+  };
+
+};
+// Initialice o Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
